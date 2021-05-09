d3a9d9dc0cdce75e8d39b93a7b532f696457cf93
OUATTARA Basseydou <bouattara-reseau@tibco.fr>
2021-05-09 13:31:53 +0200

    SW_last_changements

--- sw.js
+++ sw.js
@@ -3,5 +3,5 @@ importScripts('index.umd.js');
 self.addEventListener('fetch', (event) => {
   // Alternatively, examine event.request and only use the
   // appcachePolyfill.handle() logic for a subset of requests.
-  event.respondWith(appcachePolyfill.handle(event));
+  event.respondWith(self.appcachePolyfill.handle(event));
 });
